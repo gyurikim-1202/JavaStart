ac31a2e6bf7fd5cb806fad7ca51dc4d5e50b384c
gyuri kim <2december@naver.com>
2023-04-12 16:51:00 +0900

    출력 테스트

---
+++ ch00/sec00/test01.java
@@ -0,0 +1,14 @@
+package ch00.sec00;
+
+public class test01 {
+
+	public static void main(String[] args) {
+		// 문자 출력
+		System.out.println("Hello Java");
+		System.out.println("한글자바");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+
+	}
+
+}
