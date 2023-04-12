68f5aabdab79229bba4469b60cc52e311cb253a9
gyuri kim <2december@naver.com>
2023-04-12 16:56:58 +0900

    덧셈연습

---
+++ ch00/sec01/Test02.java
@@ -0,0 +1,15 @@
+package ch00.sec01;
+
+public class Test02 {
+	
+	public static void main(String[] args) {
+		//  덧셈연습
+		int a=5;
+		
+		int b =3 ;
+		
+		System.out.println("a+b="+(a+b));
+
+	}
+	
+}
